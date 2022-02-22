cc46a207123bd1c274575358ef511b8b571c055e
ejaquez <ejaquez@sifac.cat>
2022-02-22 09:07:17 +0100

    commit final imagenes

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,22 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPost = ({data}) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
       <Layout pageTitle={data.mdx.frontmatter.title}>
         <p>{data.mdx.frontmatter.date}</p>
+        <GatsbyImage
+          image={image}
+          alt={data.mdx.frontmatter.hero_image_alt}
+        />
+        <p>
+          Photo Credit:{" "}
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </p>
         <MDXRenderer>
           {data.mdx.body}
         </MDXRenderer>
@@ -20,6 +30,13 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
